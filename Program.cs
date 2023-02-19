6edc108fee139279ad39f069495b57d1b2970546
shahdarman <121302502+shahdarman@users.noreply.github.com>
2023-02-19 22:51:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Please Enter Your Degree");
+
+
+double degree = double.Parse(Console.ReadLine());
+
+
+
+if (degree >= 90)
+{
+    if (degree <= 100)
+    {
+        Console.WriteLine("your Grade Is Exllent"); 
+    }
+       
+}
+else if (degree >= 70 ) 
+{
+    if (degree < 90)
+    {
+      Console.WriteLine("your Grade Is VerryGood");
+    }
+}
+else if (degree < 70)
+{
+    if (degree >= 50)
+    {
+       Console.WriteLine("your Grade Is pass");
+    }
+}
+else if (degree < 50)
+{
+     Console.WriteLine("your Grade Is Fail");
+
+}
+Console.ReadKey();
+
+
